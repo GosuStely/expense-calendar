f16cd64babbce62f7d0fe408fd6a5ad968fa8e25
GosuStely <stely.tishelov@gmail.com>
2024-08-29 20:25:04 +0200

    updated layout

--- frontend/src/components/Category.jsx
+++ frontend/src/components/Category.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom'
 
 export default function Category({ category, image, location }) {
     return (
-        <Link to={`/${location}`} className="bg-white flex flex-col justify-center place-items-center w-4/5 p-5 border-2 border-slate-600 rounded-lg" >
-            <h2 className='p-2'>{category}</h2>
+        <Link to={`/${location}`} className="bg-white flex flex-col justify-center place-items-center h-5/6 w-4/5 p-5 border-2 border-slate-600 rounded-lg" >
+            <h2 className='h-1/2'>{category}</h2>
             <img src={image} alt={category} className='w-2/3' />
         </Link >
     )
